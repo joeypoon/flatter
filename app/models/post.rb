4b2301f4b19bffec95009fd286d62f98b8238337
Joey Poon <joey@joeypoon.com>
2015-06-12 09:46:43 -0500

    Add email uniqueness validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   mount_uploader :photo, PhotoUploader
 
-  validates :content, presence: true, length: { maximum: 120 }
+  validates :content, presence: true, length: { maximum: 140 }
 
   def self.search(search)
     if (search.present?)
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   mount_uploader :photo, PhotoUploader
 
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :password, presence: true
   validates :password_confirmation, presence: true
 
